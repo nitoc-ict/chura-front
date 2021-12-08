0100291e5a4b3ec831e9e691c02a4e6cba7b04c7
Takaki Hamahiga <2tf2003@gmail.com>
2021-12-08 19:18:38 +0900

    [add] Presenter

---
+++ src/components/CharacterButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <button @click="onClick">{{ character_id }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Character',
+  props: {
+    character_id: String
+  },
+  methods: {
+    onClick: function() {
+      this.$emit("onSelectCharacter", this.character_id);
+    }
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,62 @@
+<template>
+<div>
+  <div class="debug">
+    <div class="character-id-view">
+      Character ID: {{ character_id }}
+    </div>
+  </div>
+
+  <div class="character-buttons">
+    <div v-for="character in characters" :key="character.id">
+      <CharacterButton @onSelectCharacter="onSelectCharacter" :character_id=character></CharacterButton>
+    </div>
+  </div>
+
+  <div class="character-view">
+    <CharacterView></CharacterView>
+  </div>
+
+  <div class="progress-tree-chart">
+    <ProgressTreeChart></ProgressTreeChart>
+  </div>
+</div>
+</template>
+
+<script>
+import CharacterButton from "@/components/CharacterButton.vue";
+import CharacterView from "@/components/CharacterView.vue";
+import ProgressTreeChart from "@/components/ProgressTreeChart.vue"
+
+export default {
+  name: 'Home',
+  components: {
+    CharacterButton,
+    CharacterView,
+    ProgressTreeChart
+  },
+  props: {
+    character_id: {
+      type: String,
+      default: "1",
+      require: true
+    },
+  },
+  // ダミーデータ（Firebaseを使用する）
+  data: function() {
+    return {
+      characters: [
+        "vue",
+        "java",
+        "php",
+        "haskell",
+        "ruby"
+      ]
+    }
+  },
+  methods: {
+    onSelectCharacter(character_id) {
+      this.character_id = character_id;
+    }
+  }
+}
+</script>
