7931f1addba6b4601ca513a268c1a5fdc53a579b
Takaki Hamahiga <2tf2003@gmail.com>
2021-12-09 12:56:40 +0900

    [add] Dependency Injection

--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import { CharacterApplicationService } from "./application/CharacterApplicationService";
+import { CharacterRepository } from "./infrastructure/repository/CharacterRepository";
+import { InjectionConfig } from "./views/controller/InjectionConfig";
+import { CharacterFactory } from './infrastructure/CharacterFactory';
+
+const characterFactory = new CharacterFactory();
+const characterRepository = new CharacterRepository();
+
+const characterApplication = new CharacterApplicationService(
+  characterRepository,
+  characterFactory
+);
+
+let di = InjectionConfig.getInstance();
+di.injectCharacterApplication(characterApplication); //依存性注入
 
 Vue.config.productionTip = false
 
