f2d156a7acc3777f6fa0b69543d2bc05cbb8b833
cmyUE-nasu(DeskTop) <kaitomikkumiku+github@gmail.com>
2021-12-12 09:56:22 +0900

    [fix] TaskId.jsを追加。Skill.jsで使用するように変更。
    前回のコミットに反映するのを忘れていました。

--- src/domain/skilltree/Skill.js
+++ src/domain/skilltree/Skill.js
@@ -4,13 +4,13 @@ export class Skill {
     /**
      * @param {String} title
      * @param {String} description
-     * @param {Array<Task>} tasks
+     * @param {Array<TaskId>} taskIds
      * @param {Array<Skill>} dependentSkills
      */
-    constructor(title, description, tasks, dependentSkills) {
+    constructor(title, description, taskIds, dependentSkills) {
         this.title = title;                     //スキルの名前
         this.description = description;         //スキルの説明
-        this.tasks = tasks;                     //やるべきタスク
+        this.taskIds = taskIds;                 //やるべきタスク
         this.dependentSkills = dependentSkills  //依存しているスキル(このスキルを覚える上で、理解しているべきスキル。スキルツリーの根本側で、自分と直接繋がっているスキル)
     }
 }
