1d397bc0fd0b3d53dca137f59df388b29e145501
cmyUE-nasu(DeskTop) <kaitomikkumiku+github@gmail.com>
2021-12-12 23:24:33 +0900

    [fix] dummySkillTree.jsonの中身が間違っていたので修正しました。
    Task.jsをtaskIdを使用するように変更しました。

--- src/domain/skilltree/Task.js
+++ src/domain/skilltree/Task.js
@@ -2,11 +2,13 @@
 
 export class Task {
     /**
+     * @param {TaskId} taskId
      * @param {String} title
      * @param {String} description
      * @param {Boolean} isDone
      */
-    constructor(title, description,isDone) {
+    constructor(taskId, title, description,isDone) {
+        this.taskId = taskId;           //タスクの識別子
         this.title = title;             //タスクのタイトル
         this.description = description; //タスクの説明
         this.isDone = isDone;           //タスクを終了したか
