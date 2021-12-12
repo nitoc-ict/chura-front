364ccb06f6c3aa80075af203c5bd492fb4b69f30
nitf <2tf2003@gmail.com>
2021-12-12 18:00:05 +0900

    [fix] Skilドメインの修正
    IDを追加、ディレクトリを修正、ValueObject追加

--- src/domain/skilltree/skill/Skill.js
+++ src/domain/skilltree/skill/Skill.js
@@ -1,13 +1,20 @@
-//@ts-check
+//@ts-check\
+/* eslint-disable no-unused-vars */
+import { Desctiption } from "./value/SkillDescription.js";
+import TaskId from "../task/value/TaskId.js";
+import { SkillTitle } from "./value/SkillTitle.js";
+import { SkillId } from "./value/SkillId.js";
 
 export class Skill {
     /**
-     * @param {String} title
-     * @param {String} description
+     * @param {SkillId} id
+     * @param {SkillTitle} title
+     * @param {Desctiption} description
      * @param {Array<TaskId>} taskIds
      * @param {Array<Skill>} dependentSkills
      */
-    constructor(title, description, taskIds, dependentSkills) {
+    constructor(id, title, description, taskIds, dependentSkills) {
+        this.id = id;
         this.title = title;                     //スキルの名前
         this.description = description;         //スキルの説明
         this.taskIds = taskIds;                 //やるべきタスク
---
+++ src/domain/skilltree/skill/value/SkillDescription.js
@@ -0,0 +1,10 @@
+//@ts-check
+
+export class SkillDesctiption {
+    /**
+     * @param {String} value
+     */
+    constructor(value) {
+        this.value = value;
+    }
+}
