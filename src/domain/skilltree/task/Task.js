ef79352b8fe04608dd51a6b53b649f18c3c3b93e
nitf <2tf2003@gmail.com>
2021-12-12 17:59:39 +0900

    [fix] Taskドメインの修正
    IDを追加、ディレクトリを修正、ValueObjectを追加

---
+++ src/domain/skilltree/task/Task.js
@@ -0,0 +1,23 @@
+//@ts-check
+/* eslint-disable no-unused-vars */
+import { TaskDescription } from "./value/TaskDescription";
+import { TaskId } from "./value/TaskId";
+import { TaskTitle } from "./value/TaskTitle";
+
+export class Task {
+    /**
+     * @param {TaskId} id
+     * @param {TaskTitle} title
+     * @param {TaskDescription} description
+     * @param {Boolean} isDone
+     */
+    constructor(id, title, description,isDone) {
+        if (!(id instanceof TaskId)) throw "ID is not TaskId.";
+        if (!(title instanceof TaskTitle)) throw "Title is not TaskTitle.";
+        if (!(description instanceof TaskDescription)) throw "Description is not TaskDescription.";
+        this.id = id;
+        this.title = title;             //タスクのタイトル
+        this.description = description; //タスクの説明
+        this.isDone = isDone;           //タスクを終了したか
+    }
+}
---
+++ src/domain/skilltree/task/value/TaskTitle.js
@@ -0,0 +1,10 @@
+//@ts-check
+
+export class TaskTitle {
+    /**
+     * @param {String} value
+     */
+    constructor(value) {
+        this.value = value;
+    }
+}
