47dcaed5587fc225d169d8b2906b7d6c4523c319
cmyUE-nasu(DeskTop) <kaitomikkumiku+github@gmail.com>
2021-12-12 22:59:25 +0900

    [add] SkillTreeを保存する際のJsonデータの形式を決めました。
    staticディレクトリにダミーのJsonを置いておきました。
    Skill.jsで、自分のIdを持つように変更しました。
    SkillId.jsを追加しました。

--- src/domain/skilltree/Skill.js
+++ src/domain/skilltree/Skill.js
@@ -2,15 +2,17 @@
 
 export class Skill {
     /**
+     * @param {SkillId}skillId
      * @param {String} title
      * @param {String} description
      * @param {Array<TaskId>} taskIds
-     * @param {Array<Skill>} dependentSkills
+     * @param {Array<SkillId>} dependentSkillIds
      */
-    constructor(title, description, taskIds, dependentSkills) {
-        this.title = title;                     //スキルの名前
-        this.description = description;         //スキルの説明
-        this.taskIds = taskIds;                 //やるべきタスク
-        this.dependentSkills = dependentSkills  //依存しているスキル(このスキルを覚える上で、理解しているべきスキル。スキルツリーの根本側で、自分と直接繋がっているスキル)
+    constructor(skillId, title, description, taskIds, dependentSkillIds) {
+        this.skillId = skillId;                     //スキルの識別子
+        this.title = title;                         //スキルの名前
+        this.description = description;             //スキルの説明
+        this.taskIds = taskIds;                     //やるべきタスク
+        this.dependentSkillIds = dependentSkillIds  //依存しているスキル(このスキルを覚える上で、理解しているべきスキル。スキルツリーの根本側で、自分と直接繋がっているスキル)
     }
 }
