3ed93c2e6529334baec2b04bf9de0bb0f45f5f64
nitf <2tf2003@gmail.com>
2021-12-15 08:39:12 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,8 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
-
-<script>
-export default {
-  name: 'App'
-}
-</script>
 
 <style>
 #app {
